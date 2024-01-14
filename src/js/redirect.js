a0735c3bcba8c27278574e85f8097784899bc607
Edoardo Tosin <7118135+EdoardoTosin@users.noreply.github.com>
2024-01-14 16:57:38 +0100

    Fix indentation

--- src/js/redirect.js
+++ src/js/redirect.js
@@ -2,15 +2,15 @@ var storage = chrome.storage.sync || chrome.storage.local;
 
 // Replace */j/* or */s/* in the url with */wc/join/* or */wc/*/start/
 (function redirect() {
-   storage.get("toggle").then(function(items) {
-       if (items.toggle === "true") {
-           const pathParts = window.location.pathname.split('/');
-           const secondPart = pathParts[1];
-           if (secondPart !== null && ['j', 's'].includes(secondPart)) {
-               const substr = secondPart === 'j' ? ["/wc/join/", ""] : ["/wc/", "/start"];
-               const newPath = "https://" + window.location.hostname + substr[0] + pathParts[2] + substr[1] + window.location.search;
-               window.location.assign(newPath);
-           }
-       }
-   });
+    storage.get("toggle").then(function(items) {
+        if (items.toggle === "true") {
+            const pathParts = window.location.pathname.split('/');
+            const secondPart = pathParts[1];
+            if (secondPart !== null && ['j', 's'].includes(secondPart)) {
+                const substr = secondPart === 'j' ? ["/wc/join/", ""] : ["/wc/", "/start"];
+                const newPath = "https://" + window.location.hostname + substr[0] + pathParts[2] + substr[1] + window.location.search;
+                window.location.assign(newPath);
+            }
+        }
+    });
 })();
