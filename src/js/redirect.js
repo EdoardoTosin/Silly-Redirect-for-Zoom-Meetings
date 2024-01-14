3e7844f5a73bfb2e87cc7af2b07b7695eda9217c
Edoardo Tosin <7118135+EdoardoTosin@users.noreply.github.com>
2024-01-14 16:21:48 +0100

    Code refactoring

--- src/js/redirect.js
+++ src/js/redirect.js
@@ -1,13 +1,16 @@
-var storage = chrome.storage.sync;
-if (!storage) storage = chrome.storage.local;
+var storage = chrome.storage.sync || chrome.storage.local;
 
 // Replace */j/* or */s/* in the url with */wc/join/* or */wc/*/start/
-(function redirect(){
-  storage.get("toggle", function(items) {
-	  //console.log(items.toggle);
-      if (items.toggle=="true" && window.location.pathname!=null && ['j','s'].indexOf(window.location.pathname.split('/')[1])+1!=0){
-        var substr = (window.location.pathname.split('/')[1]=='j') ? ["/wc/join/",""] : ["/wc/","/start"];
-        window.location.assign("https://" + window.location.hostname + substr[0] + window.location.pathname.split('/')[2] + substr[1] + window.location.search);
-      }
-  });
+(function redirect() {
+   storage.get("toggle").then(function(items) {
+       if (items.toggle === "true") {
+           const pathParts = window.location.pathname.split('/');
+           const secondPart = pathParts[1];
+           if (secondPart !== null && ['j', 's'].includes(secondPart)) {
+               const substr = secondPart === 'j' ? ["/wc/join/", ""] : ["/wc/", "/start"];
+               const newPath = "https://" + window.location.hostname + substr[0] + pathParts[2] + substr[1] + window.location.search;
+               window.location.assign(newPath);
+           }
+       }
+   });
 })();
